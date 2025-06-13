3065e2e769f01b640f517309833bb15d0f83855d
1611navneetkumar@gmail.com <1611navneetkumar@gmail.com>
2025-06-14 01:38:27 +0530

    .

---
+++ RrvisionArrya/Arryalist.java
@@ -0,0 +1,13 @@
+package RrvisionArrya;
+
+import java.util.Arrays;
+
+public class Arryalist {
+      public static void main(String[] args) {
+            int[]arr  ={30,10,40,23,89,64};
+            for (int i = 0; i < arr.length; i++) {
+                  System.out.println(arr[i]+ " ");
+            }
+            Arrays.sort(arr);
+      }
+}
