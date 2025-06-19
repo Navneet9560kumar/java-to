f7a917ebc95393a04abaf9c664db55087a79d5ad
1611navneetkumar@gmail.com <1611navneetkumar@gmail.com>
2025-06-19 16:02:06 +0530

    sum of set DP & robs perblom

---
+++ DynamicProgramming/SubsetSumDpAditiya.java
@@ -0,0 +1,41 @@
+package DynamicProgramming;
+
+public class SubsetSumDpAditiya {
+
+
+         public static boolean subset(int[] arr, int sum) {
+        int n = arr.length;
+        boolean[][] t = new boolean[n + 1][sum + 1];
+
+        // Initialization
+        for (int i = 0; i <= n; i++) {
+            t[i][0] = true; // sum = 0 is always possible with empty subset
+        }
+
+        for (int j = 1; j <= sum; j++) {
+            t[0][j] = false; // with 0 elements, positive sum is not possible
+        }
+
+        // DP Table Filling
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= sum; j++) {
+                if (arr[i - 1] <= j) {
+                    t[i][j] = t[i - 1][j] || t[i - 1][j - arr[i - 1]];
+                } else {
+                    t[i][j] = t[i - 1][j];
+                }
+            }
+        }
+
+        return t[n][sum];
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 7, 8, 10};
+        int sum = 11;
+
+        boolean result = subset(arr, sum);
+
+        System.out.println("Is subset with sum " + sum + " possible? " + result);
+    }
+}
--- DynamicProgramming/robss_198.java
+++ DynamicProgramming/robss_198.java
@@ -17,7 +17,19 @@ public class robss_198 {
             // dp[i] will store the value of amolunt(i) 
             int[] dp = new int[nums.length]; Arrays.fill(dp,-1);
         return amount(nums, 0,dp);
-    }
+    }//Tabulastion method 🐧🐧🐧🐧 
+
+      public static int robs(int[] arr){
+        int n= arr.length;
+        if(n==1) return arr[0];
+        int[]dp= new int[n];
+        dp[0] = arr[0];
+        dp[1] = Math.max(arr[0],arr[1]);
+        for (int i = 2; i < n; i++) {
+            dp[1]= Math.max(arr[i]+dp[i+2],dp[i-1]);
+        }
+        return dp[n-1];
+      }
 
     public static void main(String[] args) {
       
