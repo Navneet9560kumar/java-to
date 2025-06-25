924b642f772ac22d893ebea32d6f8cb7ab601ef7
1611navneetkumar@gmail.com <1611navneetkumar@gmail.com>
2025-06-26 02:12:03 +0530

    Arry revision

--- RrvisionArrya/Rotatearr.java
+++ RrvisionArrya/Rotatearr.java
@@ -21,5 +21,31 @@ public class Rotatearr {
                   j--;
             }
 
+            // agr K elemnt se rotate karna ho to aase karege 
+
+//              public void rotate(int[] nums, int k) {
+//          int n = nums.length;
+//     k = k % n; // In case k > n
+
+//     // Step 1: Reverse entire array
+//     reverse(nums, 0, n - 1);
+
+//     // Step 2: Reverse first k elements
+//     reverse(nums, 0, k - 1);
+
+//     // Step 3: Reverse remaining elements
+//     reverse(nums, k, n - 1); 
+//     }
+
+//     private void reverse(int[] nums, int start, int end) {
+//     while (start < end) {
+//         int temp = nums[start];
+//         nums[start] = nums[end];
+//         nums[end] = temp;
+//         start++;
+//         end--;
+//     }
+//     }
+
       }
 }
