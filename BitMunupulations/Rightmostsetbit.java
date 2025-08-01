03328a31c4f9f6451b1c66882a547c492f87a874
1611navneetkumar@gmail.com <1611navneetkumar@gmail.com>
2025-08-01 19:16:06 +0530

    .datahedling project

---
+++ BitMunupulations/Rightmostsetbit.java
@@ -0,0 +1,11 @@
+package BitMunupulations;
+
+public class Rightmostsetbit {
+
+      public static int getRightmostSetBit(int n) {
+            return n & -n;
+      }
+      public static void main(String[] args) {
+            
+      }
+}
--- BitMunupulations/Trunonkbit.java
+++ BitMunupulations/Trunonkbit.java
@@ -1,8 +1,17 @@
 package BitMunupulations;
 
 public class Trunonkbit {
-      public static void main(String[] args) {
-            int[] a = {10,9,8,7,6,5,4,3,2,1,0};
-            
+
+      public static int ToggleKthbit(int n, int k){
+            return n ^ (1 << k);
       }
+
+            public static void main(String[] args) {
+            int[] a = {10,9,8,7,6,5,4,3,2,1,0};
+            int k = 2; // Example: Toggle 2nd bit
+            for (int i : a) {
+                  System.out.println("Original: " + i + ", Toggled: " + ToggleKthbit(i, k));
+            }
+      }
+
 }
