5bd7b26e8b3c3fd6e98f03f7923933334051b0d9
1611navneetkumar@gmail.com <1611navneetkumar@gmail.com>
2025-08-13 20:43:25 +0530

    .

---
+++ string/RevisonString/updateEvenPostion.java
@@ -0,0 +1,16 @@
+package string.RevisonString;
+
+public class updateEvenPostion {
+      public static void main(String[] args) {
+            String  s= "Physics wallh Skills";
+            String str = "";
+            for (int i = 0; i < s.length(); i++) {
+                  if(i%2==0) str += 'a';
+                  else str += s.charAt(i);
+
+            }
+            s = str;
+            System.out.println(s);
+         
+      }
+}
