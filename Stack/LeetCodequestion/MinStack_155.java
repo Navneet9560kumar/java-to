b5f8a51a56491440a396cb79d7d1bb5a7a864c6c
1611navneetkumar@gmail.com <1611navneetkumar@gmail.com>
2025-10-05 13:57:12 +0530

    .

--- Stack/LeetCodequestion/MinStack_155.java
+++ Stack/LeetCodequestion/MinStack_155.java
@@ -4,16 +4,41 @@ import java.util.*;
 
 public class MinStack_155 {
        Stack<Integer>st= new Stack<>();
+      Stack<Integer>min = new Stack<>();
 
-      public void push(int val){
+      class MinStack {
+  
 
+    public MinStack() {
+        
+    }
+    
+    public void push(int val) {
+      if(st.size()==0){
+            st.push(val);
+            min.push(val);
+      }else{
+            st.push(val);
+            if(min.peek()<val) min.push(min.peek());
+            else min.push(val);
       }
+        
+    }
+    
+    public void pop() {
+        st.pop();
+            min.pop();
+    }
+    
+    public int top() {
+        return st.peek();
+    }
+    
+    public int getMin() {
+        
+    }
+}
 
-      public void pop(int val){
-            if(!st.isEmpty() && st.peek() == val){
-                  st.pop();
-            }
-      }
       public static void main(String[] args) {
             
       }
