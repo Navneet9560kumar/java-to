4926c4bf5001c61fac40efd0ef128afdba4025dc
1611navneetkumar@gmail.com <1611navneetkumar@gmail.com>
2025-11-11 23:01:22 +0530

    Complite heap

---
+++ Heaps/HeapPart2/ImplementionsHeap_01.java
@@ -0,0 +1,24 @@
+package Heaps.HeapPart2;
+
+
+
+class Minheap{
+      int []arr;
+      int size = 0;
+      Minheap(int capacity){
+            arr = new int[capacity];
+            size = 0;
+
+      }
+      public void add(int num){
+            
+      }
+}
+public class ImplementionsHeap_01 {
+
+
+     public static void main(String[] args) {
+      // min heap implementation
+      Minheap pq = new Minheap()
+     } 
+}
